2ff8a9d83aa0f610c99036c91e8d85eea75c860f
Jacob Granger <52899035+Jacob-granger@users.noreply.github.com>
2023-08-04 10:18:55 +1200

    Add HTTP request threads

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import * as Path from 'node:path'
 
 import express from 'express'
 import hbs from 'express-handlebars'
-
+import { getThreads } from './utils.js'
 
 const server = express()
 
@@ -22,50 +22,65 @@ server.set('views', Path.resolve('server/views'))
 /// PSEUDO ///
 
 // THREADS OBJECT (its monolith time, baybeeee!!)
-    //  { id: NUMBER, title: STRING, comments: ARRAY[ {name: STRING, date: DATE OBJECT, comment: STRING} ]}
-
+//  { id: NUMBER, title: STRING, comments: ARRAY[ {name: STRING, date: DATE OBJECT, comment: STRING} ]}
 
 // HOME PAGE //
-    // GET '/' 
-        // HOME PAGE, button to go to threads.
-        // BUTTON -> go to threads list ('/threads')
-        // render('home') -- 'FIGMA SCREEN 1'
-       server.get('/', (req, res) => {
-        res.render('home')
-       })
+// GET '/'
+// HOME PAGE, button to go to threads.
+// BUTTON -> go to threads list ('/threads')
+// render('home') -- 'FIGMA SCREEN 1'
+server.get('/', (req, res) => {
+  res.render('home')
+})
 
-// THREADS ROUTER // 
-    // GET '/threads'
-        // List of all threads / topics
-        // Clicking each thread will pass an ID that takes user to selected thread page (e.g threads/3)
-            // READ FILE utility -> read from threads and map to list items with links to individual threads
-            // OBJECT threads data
-            // render('threads', data) -- 'FIGMA SCREEN 2' list of all the thread topics a user can choose from (EACH LOOP)
+// THREADS ROUTER //
+// GET '/threads'
+// List of all threads / topics
+// Clicking each thread will pass an ID that takes user to selected thread page (e.g threads/3)
+// READ FILE utility -> read from threads and map to list items with links to individual threads
+// OBJECT threads data
+// render('threads', data) -- 'FIGMA SCREEN 2' list of all the thread topics a user can choose from (EACH LOOP)
 
-    // GET '/threads/:id'
-        // on comment form submit, POST req with req.body to /threads/:id/add-comment vvvv
-        // READ FILE utility -> read from singular thread and display comments
-        // OBJECT contains information related to thread with "id"
-        // render('thread-view', data) -- 'FIGMA SCREEN 3' list of all comments related to thread + form to add a comment (EACH LOOP)
+// data.threadds [] of threads , each has a comments array
+//  { id: 1,
+// title: 'The moon has a dark side, CMV.',
+// likes: 0,
+// comments: 2 }
+// array of threads, containing all thread info, comments are reduced to a number
 
-    // POST '/threads/:id/add-comment' (POST ONLY, comment for a thread topic)
-        // add comment to our 'db' or JSON
-        // READ file
-        // Find ID of thread user is adding a coment to
-        // Create new comment object
-        // push(newComment) to thread
-        // write to file
-        // redirect to /threads/:id on success
-            // work with form 
+server.get('/threads', async (req, res) => {
+  const data = await getThreads()
+  const threadsArr = data.threads.map((thread) => {
+    thread.comments = thread.comments.length
+    return thread
+  })
+  console.log(threadsArr)
+  //   res.render('threads', threadsArr)
+})
+
+// GET '/threads/:id'
+// on comment form submit, POST req with req.body to /threads/:id/add-comment vvvv
+// READ FILE utility -> read from singular thread and display comments
+// OBJECT contains information related to thread with "id"
+// render('thread-view', data) -- 'FIGMA SCREEN 3' list of all comments related to thread + form to add a comment (EACH LOOP)
+
+// POST '/threads/:id/add-comment' (POST ONLY, comment for a thread topic)
+// add comment to our 'db' or JSON
+// READ file
+// Find ID of thread user is adding a coment to
+// Create new comment object
+// push(newComment) to thread
+// write to file
+// redirect to /threads/:id on success
+// work with form
 
 // -STRETCH
 
 // THREADS STRETCH
-    // GET '/threads/new'
-        // 
+// GET '/threads/new'
+//
 
 // .get('threads/new') -> new thread form
 // .post('threads/new') -> post for new thread, redirects to new thread on success
-
 
 export default server
