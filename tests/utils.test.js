82c3c286927000b620257778eb9d333a6c41c19d
Lari Ruecker <122192082+popoxta@users.noreply.github.com>
2023-08-04 09:47:44 +1200

    Add getThreads test stubs

---
+++ tests/utils.test.js
@@ -0,0 +1,9 @@
+import { expect, test, vi, describe, beforeAll } from 'vitest'
+import { render } from '../test-utils.js'
+
+import request from 'supertest'
+
+describe('Reading data', () => {
+  test.skip('getThreads should return an object containing all threads')
+  test.skip('getThreads should initialize a read operation')
+})
